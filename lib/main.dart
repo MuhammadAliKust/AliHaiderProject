2d0aca6c0827adb87ca90c76424565f45e178013
Muhammad Ali Nizami <kustalics@gmail.com>
2024-11-12 11:50:44 +0500

    Multiple Selection Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ali_haider_project/list_view_demo.dart';
+import 'package:ali_haider_project/multi_selection_view.dart';
 import 'package:ali_haider_project/single_selection_view.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SingleSelectionListViewDemo()
+      home: MultiSelectionListViewDemo()
     );
   }
 }
---
+++ lib/multi_selection_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MultiSelectionListViewDemo extends StatefulWidget {
+  MultiSelectionListViewDemo({super.key});
+
+  @override
+  State<MultiSelectionListViewDemo> createState() =>
+      _MultiSelectionListViewDemoState();
+}
+
+class _MultiSelectionListViewDemoState
+    extends State<MultiSelectionListViewDemo> {
+  List<int> selectedIndex = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Multi Selection List View Demo"),
+        ),
+        body: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, i) {
+              return ListTile(
+                onTap: () {
+                  if(selectedIndex.contains(i)){
+                    selectedIndex.remove(i);
+                  }else{
+                    selectedIndex.add(i);
+                  }
+
+                  setState(() {});
+                },
+                leading: Icon(Icons.notifications),
+                title: Text("Loop Index $i"),
+                subtitle: Text("Selected Index: $selectedIndex"),
+                trailing: Icon(Icons.arrow_forward_ios),
+                tileColor:
+                    selectedIndex.contains(i) ? Colors.blue : Colors.white,
+              );
+            }));
+  }
+}
